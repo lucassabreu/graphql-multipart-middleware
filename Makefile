58901dae90b3de236bbb5d6897711910c5cc4aab
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-06-17 19:57:06 -0300

    validations

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+all: help
 
 export PORT ?= 8000
 
@@ -5,12 +6,24 @@ export PORT ?= 8000
 help: ## show this help
  	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-install:
-	go get -u -v ./...
+install: ## install package dependencies
+	go get -u -v -t ./...
 
-serve-example:
+serve-example: ## start the example server
 	go run examples/main.go
 
-serve-watch-example:
+serve-watch-example: ## start the example server watching for changes
 	go get github.com/codegangsta/gin
 	PORT=8001 gin --port ${PORT} --appPort 8001 --build ./examples
+
+tests: ## run the package's tests
+	go test -v -race .
+
+coverage: ## calcs the coverage for the package
+	go get golang.org/x/tools/cmd/cover
+	go get github.com/mattn/goveralls
+	go test -v -covermode=count -coverprofile=coverage.out
+
+send-statistics: ## send statistics
+	goveralls -coverprofile=coverage.out -service=travis-ci -repotoken ${COVERALLS_TOKEN}
+
---
+++ handler_test.go
@@ -0,0 +1,196 @@
+package graphqlmultipart_test
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	graphqlmultipart "github.com/lucassabreu/graphql-multipart-middleware"
+	"github.com/lucassabreu/graphql-multipart-middleware/testutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMultipartMiddleware_ForwardsRequestsOtherThanMultipart(t *testing.T) {
+	newRequest := func(contentType string) *http.Request {
+		r, _ := http.NewRequest("GET", "/graphql", strings.NewReader(""))
+		r.Header.Set("Content-type", contentType)
+		return r
+	}
+
+	cases := map[string]*http.Request{
+		"application/json":                  newRequest("application/json"),
+		"application/graphql":               newRequest("application/graphql"),
+		"application/x-www-form-urlencoded": newRequest("application/x-www-form-urlencoded"),
+	}
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("reached me"))
+	})
+
+	for n, r := range cases {
+		t.Run(n, func(t *testing.T) {
+			resp := httptest.NewRecorder()
+
+			mh := graphqlmultipart.NewHandler(
+				&testutil.Schema,
+				1*1024,
+				h,
+			)
+
+			mh.ServeHTTP(resp, r)
+
+			body, _ := ioutil.ReadAll(resp.Result().Body)
+
+			require.Equal(t, string(body), "reached me")
+		})
+	}
+}
+
+func newFileUploadRequest(params map[string]string, files map[string]string) *http.Request {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+
+	for paramName, path := range files {
+		file, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		fileContents, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+		fi, err := file.Stat()
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+
+		part, err := writer.CreateFormFile(paramName, fi.Name())
+		if err != nil {
+			panic(err)
+		}
+		part.Write(fileContents)
+	}
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+
+	if err := writer.Close(); err != nil {
+		panic(err)
+	}
+
+	r, _ := http.NewRequest("POST", "/graphql", body)
+	r.Header.Set("Content-Type", writer.FormDataContentType())
+	return r
+}
+
+func getJSONError(m string, v ...interface{}) string {
+	if len(v) > 0 {
+		m = fmt.Sprintf(m, v...)
+	}
+
+	return fmt.Sprintf(
+		"{\"data\":null,\"errors\": [{\"message\":%s, \"locations\":[]}]}",
+		strconv.Quote(m),
+	)
+}
+
+func TestHandlerShouldValidateRequest(t *testing.T) {
+	type test struct {
+		req   *http.Request
+		respo string
+	}
+	cases := map[string]test{
+		"missing_operation_field": test{
+			req:   newFileUploadRequest(make(map[string]string), make(map[string]string)),
+			respo: getJSONError(graphqlmultipart.OperationsFieldMissingMessage),
+		},
+		"missing_map_field": test{
+			req: newFileUploadRequest(
+				map[string]string{"operations": "{}"},
+				make(map[string]string),
+			),
+			respo: getJSONError(graphqlmultipart.MapFieldMissingMessage),
+		},
+		"invalid_operaction_field": test{
+			req: newFileUploadRequest(
+				map[string]string{
+					"operations": "{}",
+					"map":        "{}",
+				},
+				make(map[string]string),
+			),
+			respo: getJSONError(graphqlmultipart.InvalidOperationsFieldMessage),
+		},
+		"invalid_operaction_field_empty_array": test{
+			req: newFileUploadRequest(
+				map[string]string{
+					"operations": "[]",
+					"map":        "{}",
+				},
+				make(map[string]string),
+			),
+			respo: getJSONError(graphqlmultipart.InvalidOperationsFieldMessage),
+		},
+		"missing_file": test{
+			req: newFileUploadRequest(
+				map[string]string{
+					"operations": "[{\"query\":\"query hero{id}\",\"variables\":{}}]",
+					"map":        "{\"file\":[]}",
+				},
+				make(map[string]string),
+			),
+			respo: "[" + getJSONError(graphqlmultipart.MissingFileMessage, "file") + "]",
+		},
+		"invalid_map_path": test{
+			req: newFileUploadRequest(
+				map[string]string{
+					"operations": "{\"query\":\"query hero{id}\",\"variables\":{}}",
+					"map":        "{\"file\":[\"variables.file\"]}",
+				},
+				map[string]string{"file": "handler.go"},
+			),
+			respo: getJSONError(graphqlmultipart.InvalidMapPathMessage, "variables.file", "file"),
+		},
+		"invalid_map_path_batching": test{
+			req: newFileUploadRequest(
+				map[string]string{
+					"operations": "[{\"query\":\"query hero{id}\",\"variables\":{}}]",
+					"map":        "{\"file\":[\"0.variables.file\"]}",
+				},
+				map[string]string{"file": "handler.go"},
+			),
+			respo: "[" + getJSONError(graphqlmultipart.InvalidMapPathMessage, "0.variables.file", "file") + "]",
+		},
+	}
+
+	mh := graphqlmultipart.NewHandler(
+		&testutil.Schema,
+		1*1024,
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Should not have forwarded the request"))
+		}),
+	)
+
+	for name, test := range cases {
+		t.Run(name, func(t *testing.T) {
+
+			resp := httptest.NewRecorder()
+
+			mh.ServeHTTP(resp, test.req)
+
+			body, _ := ioutil.ReadAll(resp.Result().Body)
+
+			require.JSONEq(t, string(body), test.respo)
+		})
+	}
+}
